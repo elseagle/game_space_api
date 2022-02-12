acd71e1134580a992b930ac4162143f82891ed1e
Sogo Ogundowole <dowolebolu@gmail.com>
2022-02-12 14:20:22 +0100

    chore: add environment variable to docker

--- start.sh
+++ start.sh
@@ -4,9 +4,8 @@ if [ "$CREATE_TABLE" = "true" ]
 then
     python3 create_table.py
 else
-  echo "$CREATE_TABLE"
-  echo "$0"
-  echo "no try again"
+  echo "Pass in environment variable for CREATE_TABLE in docker-compose.yml"
+
 fi
 
 flask run --host=0.0.0.0 --port=5500
