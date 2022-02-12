6724b81667e235fce990fd04f9d73213c5760a93
Sogo Ogundowole <dowolebolu@gmail.com>
2022-02-12 06:50:45 +0100

    refactored: space shift

--- test/test_game_combination.py
+++ test/test_game_combination.py
@@ -9,6 +9,7 @@ def test_max_space(get_games, get_pen_drive_space):
 
 
 def test_below_total_space(get_games, get_pen_drive_space):
-    game_combination = get_game_combination(get_games, get_pen_drive_space - 103000345)
+    space = get_pen_drive_space - 103000345
+    game_combination = get_game_combination(get_games, space)
     assert len(game_combination["games"]) < 5
-    assert get_pen_drive_space >= game_combination["total_space"]
+    assert space >= game_combination["total_space"]
